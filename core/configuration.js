5e343f4b69a136114fc706b74e809f482e43c78b
Ovidiu Negus <neguso@hotmail.com>
2015-12-06 20:56:48 +0200

    Home

--- core/configuration.js
+++ core/configuration.js
@@ -1,62 +1,205 @@
 'use strict';
 
 var fs = require('fs');
+var restify = require('restify');
 
 module.exports = function(file)
 {
-	let current = {};
+	let current = {}, previous = {};
+	let watchlist = [];
 
 	return {
 
-		load: function(cb) {
-			fs.access(file, fs.F_OK | fs.R_OK, function(err) {
-				if(err) return;
+		connect: function(cb)
+		{
+			fs.access(file, fs.F_OK | fs.R_OK, function(err)
+			{
+				if(err) {
+					previous = clone(current = {});
 
-				fs.readFile(file, function(err, data) {
-					if(err) cb(err);
+					cb(null);
+					watch();
+					return;
+				}
 
-					cb(null, JSON.parse(data));
+				fs.readFile(file, function(err, data)
+				{
+					if(err) {
+						cb(err);
+						return;
+					}
+
+					try {
+						previous = clone(current = JSON.parse(data));
+					}
+					catch(error) {
+						cb(error);
+						return;
+					}
+
+					cb(null);
+					watch();
 				});
 			});
 		},
 
-		set: function(path, value) {
-			parse(path, function(object, property) {
-				object[property] = value;
-			});
+		json: function(cb)
+		{
+			return JSON.stringify(current);
 		},
 
-		get: function(path, defaultValue) {
+		set: function(path, value)
+		{
+			parse(current, path, function(object, property)
+			{
+				object[property] = clone(value);
+			}, true);
+			notify(watchlist, path, current, previous, function(watch, currentvalue, previousValue)
+			{
+				watch.callback(path, currentvalue, previousValue);
+			});
+			previous = clone(current);
+		},
+
+		get: function(path, defaultValue)
+		{
 			var value = defaultValue;
-			parse(path, function(object, property) {
+			parse(current, path, function(object, property)
+			{
 				value = object[property];
 			});
 			if(typeof value === 'undefined')
 				return defaultValue;
 			else
-				return value;
+				return clone(value);
 		},
 
-		read: function()
+		save: function(cb)
 		{
-			return JSON.parse(JSON.stringify(current));
+			unwatch();
+			fs.writeFile(file, JSON.stringify(current, null, '\t'), function(err)
+			{
+				watch();
+
+				if(err) {
+					cb(err);
+					return;
+				}
+
+				cb(null);
+			});
+		},
+
+		watch: function(path, cb)
+		{
+			if(watchlist.indexOf(path) === -1)
+				watchlist.push({path: path, callback: cb});
+		},
+
+		unwatch: function(path)
+		{
+			let i = watchlist.indexOf(path);
+			if(i !== -1)
+				watchlist.splice(i, 1);
 		}
 	};
 
 
-	function parse(path, callback)
+	function parse(config, path, callback, create)
 	{
 		var ary = path.split('/');
-		var o = current;
-		for(let i = 0; i < ary.length; i++)
-		{
+		for(let i = 0; i < ary.length; i++) {
 			if(i === ary.length - 1)
-				callback(o, ary[i]);
+				callback(config, ary[i]);
 			else {
-				if (!o.hasOwnProperty(ary[i]))
-					o[ary[i]] = {};
-				o = o[ary[i]];
+				if(!config.hasOwnProperty(ary[i])) {
+					if(create)
+						config[ary[i]] = {};
+					else {
+						callback(config, ary[i]);
+						return;
+					}
+				}
+				config = config[ary[i]];
 			}
 		}
 	}
+
+	function equals(c1, c2, path)
+	{
+		var v1, v2;
+		parse(c1, path, function(object, property)
+		{
+			v1 = object[property];
+		});
+		parse(c2, path, function(object, property)
+		{
+			v2 = object[property];
+		});
+		return JSON.stringify(v1) === JSON.stringify(v2);
+	}
+
+	function notify(watchlist, path, current, previous, callback)
+	{
+		for(let i = 0; i < watchlist.length; i++) {
+			let currentValue, previousValue;
+			parse(current, path, function(object, property)
+			{
+				currentValue = object[property];
+			});
+			parse(previous, path, function(object, property)
+			{
+				previousValue = object[property];
+			});
+
+			if(watchlist[i].path.startsWith(path) && JSON.stringify(currentValue) !== JSON.stringify(previousValue))
+				callback(watchlist[i], currentValue, previousValue);
+		}
+	}
+
+	function clone(o)
+	{
+		return JSON.parse(JSON.stringify(o));
+	}
+
+	function watch()
+	{
+		fs.unwatchFile(file);
+		fs.watchFile(file, { persistent: false }, function(curr, prev)
+		{
+
+			fs.readFile(file, function(err, data)
+			{
+				if(err) return;
+
+				try {
+					current = JSON.parse(data);
+				}
+				catch(error) {
+					return;
+				}
+
+				var called = [];
+				watchlist.forEach(function(item)
+				{
+					notify(watchlist, item.path, current, previous, function(watch, currentvalue, previousValue)
+					{
+						if(called.indexOf(watch) === -1) {
+							watch.callback(watch.path, currentvalue, previousValue);
+							called.push(watch);
+						}
+					});
+				});
+
+				previous = clone(current);
+			});
+
+		});
+	}
+
+	function unwatch()
+	{
+		fs.unwatchFile(file);
+	}
+
 };
--- tests/configuration.js
+++ tests/configuration.js
@@ -1,6 +1,9 @@
 exports.test = function() {
-	load();
-
+	connect_ok_when_nofile();
+	connect_error_when_invalidfile();
+	connect_loads_values();
+	json_returns_ok();
+	save_values_ok();
 	get_returns_undefined_value();
 	get_returns_default_value();
 	set_undefined_value();
@@ -9,48 +12,102 @@ exports.test = function() {
 	set_undefined_deep_property();
 	set_existing_deep_property();
 	set_intermediate_property();
+	watch_raise_when_adding_values();
+	watch_raise_when_updating_values();
+	watch_notraise_when_settingthesame_values();
+	watch_notraise_when_saving_values();
+	watch_raise_when_file_changes();
 };
 
 var assert = require('assert');
-var config = require('../core/configuration')('config.json');
 
-function load()
+function connect_ok_when_nofile()
 {
-	config.load(function(err, data) {
+	var config = require('../core/configuration')('missing.json');
+	config.connect(function(err) {
+		assert(err === null, 'connect() should not return error when file not found');
+	});
+}
 
-		var a = data;
+function connect_error_when_invalidfile()
+{
+	var config = require('../core/configuration')('invalid.json');
+	config.connect(function(err) {
+		assert(err !== null, 'connect() should return error when file format is invalid');
+	});
+}
+
+function connect_loads_values()
+{
+	var config = require('../core/configuration')('config.json');
+	config.connect(function(err) {
+		assert(config.get('property') === 'flat', 'connect() should load correct root values');
+		assert(config.get('object1/object2/property') === 'deep', 'connect() should load correct deep values');
+	});
+}
+
+function json_returns_ok()
+{
+	var config = require('../core/configuration')('config.json');
+	var json = config.json();
+	assert(json === '{}', 'read() should return correct value');
+	config.set('property', 'value');
+	config.set('number', 100);
+	json = config.json();
+	assert(json === '{"property":"value","number":100}', 'read() should return correct value');
+}
+
+function save_values_ok()
+{
+	var fs = require('fs');
+	fs.unlink('new.json', function(err) {
+
+		var config = require('../core/configuration')('new.json');
+		config.set('property', 'value');
+		config.save(function(err) {
+			assert(err === null, 'save() should not return error');
+
+			var reconfig = require('../core/configuration')('new.json');
+			reconfig.connect(function(err) {
+				assert(reconfig.json() === '{"property":"value"}', 'save() should save correct values');
+			});
+		});
 
 	});
 }
 
 function get_returns_undefined_value()
 {
+	var config = require('../core/configuration')('config.json');
 	var v = config.get('undefinedvalue1');
 	assert(typeof v === 'undefined', 'get() should return undefined value');
 }
 
 function get_returns_default_value()
 {
+	var config = require('../core/configuration')('config.json');
 	var v = config.get('undefinedvalue2', 'default');
 	assert(v === 'default', 'get() should return default value');
 }
 
 function set_undefined_value()
 {
+	var config = require('../core/configuration')('config.json');
 	config.set('value1', 'test');
 	assert(config.get('value1') === 'test', 'set() should set value');
 }
 
 function set_existing_value()
 {
+	var config = require('../core/configuration')('config.json');
 	config.set('value1', 'initial value');
 	config.set('value1', 'test');
 	assert(config.get('value1') === 'test', 'set() should set value');
 }
 
-
 function get_returns_value()
 {
+	var config = require('../core/configuration')('config.json');
 	config.set('value2', 'test');
 	assert(config.get('value2') === 'test', 'get() should return current value');
 	assert(config.get('value2', 'default') === 'test', 'get() should return current value');
@@ -58,15 +115,16 @@ function get_returns_value()
 
 function set_undefined_deep_property()
 {
+	var config = require('../core/configuration')('config.json');
 	config.set('folder1/value', 'test');
 	assert(config.get('folder1/value') === 'test', 'set() should set value');
-
 	config.set('folder1/folder11/folder111/value', 'test');
 	assert(config.get('folder1/folder11/folder111/value') === 'test', 'set() should set value');
 }
 
 function set_existing_deep_property()
 {
+	var config = require('../core/configuration')('config.json');
 	config.set('folder2/value', 'initial value');
 	config.set('folder2/value', 'test');
 	assert(config.get('folder2/value') === 'test', 'set() should set value');
@@ -74,8 +132,128 @@ function set_existing_deep_property()
 
 function set_intermediate_property()
 {
+	var config = require('../core/configuration')('config.json');
 	config.set('folder3/folder31/value', 'test');
 	config.set('folder3/value', 'intermediate');
 	assert(config.get('folder3/value') === 'intermediate', 'set() should set value');
 	assert(config.get('folder3/folder31/value') === 'test', 'deeper set() should not affect intermediate values');
+}
+
+function watch_raise_when_adding_values()
+{
+	var config = require('../core/configuration')('config.json');
+	config.connect(function(err) {
+		if(err)
+			throw new Error('error loading config.json file');
+
+		var called = false;
+		config.watch('new property', function(path, current, previous) {
+			called = true;
+		});
+		config.set('new property', 'new value');
+		assert(called, 'watch() should invoke callback');
+	});
+}
+
+function watch_raise_when_updating_values()
+{
+	var config = require('../core/configuration')('config.json');
+	config.connect(function(err) {
+		if(err)
+			throw new Error('error loading config.json file');
+
+		var called = false;
+		config.watch('property', function(path, current, previous) {
+			called = true;
+		});
+		config.set('property', 'new value');
+		assert(called, 'watch() should invoke callback');
+	});
+}
+
+function watch_notraise_when_settingthesame_values()
+{
+	var config = require('../core/configuration')('config.json');
+	config.connect(function(err) {
+		if(err)
+			throw new Error('error loading config.json file');
+
+		var called = false;
+		config.watch('property', function(path, current, previous) {
+			called = true;
+		});
+		config.set('property', 'flat');
+		assert(!called, 'watch() should not invoke callback');
+	});
+}
+
+function watch_notraise_when_saving_values()
+{
+	var fs = require('fs');
+	fs.unlink('watch1.json', function(err) {
+
+		var config = require('../core/configuration')('watch1.json');
+		config.connect(function(err) {
+			if(err)
+				throw new Error('error loading watch1.json file');
+
+			config.set('property', 'random');
+			var called = false;
+			config.watch('property', function(path, current, previous) {
+				called = true;
+			});
+
+			config.save(function(err) {
+				if(err)
+					throw new Error('error saving watch1.json file');
+
+				setTimeout(function() {
+					assert(!called, 'watch() should not invoke callback');
+				}, 6000);
+			});
+		});
+
+	});
+}
+
+function watch_raise_when_file_changes()
+{
+	var fs = require('fs');
+	fs.unlink('watch2.json', function(err)
+	{
+
+		var config = require('../core/configuration')('watch2.json');
+		config.connect(function(err)
+		{
+			if(err)
+				throw new Error('error loading watch2.json file');
+
+			config.set('property', 'value');
+			config.save(function(err)
+			{
+				if(err)
+					throw new Error('error saving watch2.json file');
+
+				var called = false;
+				config.watch('property', function(path, current, previous)
+				{
+					called = true;
+				});
+
+				var fs = require('fs');
+				fs.writeFile('watch2.json', '{"property":"new value"}', function(err)
+				{
+					if(err)
+						throw new Error('error changing watch2.json file');
+
+					setTimeout(function()
+					{
+						assert(called, 'watch() should invoke callback');
+					}, 6000);
+				});
+
+			});
+		});
+
+	});
 }
